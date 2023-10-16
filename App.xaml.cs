b7d13111fa29f0c08d5dcb8b513af3d0a2888ac8
sandersnijder <sander.snijder@student.nhlstenden.com>
2023-10-16 11:47:09 +0200

    test commit 2

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,6 +11,7 @@ namespace GameSportschoolKees
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// /// test 2
     public partial class App : Application
     {
     }

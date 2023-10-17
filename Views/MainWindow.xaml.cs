48773e09ef2212d361ec60d34313e77042341ee2
sanvander <sander-werff@hotmail.com>
2023-10-17 12:58:08 +0200

    Add Views mapje waar alle schermen in komen

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GameSportschoolKees
+namespace GameSportschoolKees.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

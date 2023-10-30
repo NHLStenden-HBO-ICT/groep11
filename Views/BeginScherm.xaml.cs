dcb984b3b237ddad4467f83cb3d19c18cd94b338
SanderSnijder <sander.snijder@student.nhlstenden.com>
2023-10-30 11:48:18 +0100

    start beginscherm is nog niet helemaal af.

--- Views/BeginScherm.xaml.cs
+++ Views/BeginScherm.xaml.cs
@@ -10,18 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Game_Interaction.Views
+namespace Game
 {
     /// <summary>
-    /// Interaction logic for BeginScherm.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class BeginScherm : Window
+    public partial class MainWindow : Window
     {
-        public BeginScherm()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void QuitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }

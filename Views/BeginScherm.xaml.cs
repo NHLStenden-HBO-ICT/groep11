3cf439e56d54653f5b0e8e495ffaff658fab2128
SanderSnijder <sander.snijder@student.nhlstenden.com>
2023-11-02 18:54:57 +0100

    functie van de knoppen toegevoedt

--- Views/BeginScherm.xaml.cs
+++ Views/BeginScherm.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Game_interaction.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,12 +20,35 @@ namespace Game_Interaction.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class BeginScherm : Window
     {
         public BeginScherm()
         {
             InitializeComponent();
         }
+        //gaat naar het volgende scherm
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            Pregamescherm pregamescherm = new Pregamescherm();
+            pregamescherm.Show();
+            this.Close();
+        }
+        
+        private void BClick(object sender, RoutedEventArgs e)
+        {
+            Besturingscherm besturingscherm = new Besturingscherm();
+            besturingscherm.Show();
+            this.Close();
+        }
+
+
+      /* private void LeaderboardClick(object sender, RoutedEventArgs e)
+        {
+           Leaderboard Lbs = new Pregamescherm();
+            Lbs.Show();
+            this.Close();
+        } */
 
         private void QuitClick(object sender, RoutedEventArgs e)
         {
@@ -31,3 +56,5 @@ namespace Game_Interaction.Views
         }
     }
 }
+
+
